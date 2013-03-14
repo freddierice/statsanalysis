3db7abd34bd045fd1c7b899466030545ab595a21
Freddie Rice <farice14@blakeschool.org>
2013-03-14 14:26:26 -0500

    First Commit

---
+++ Stats Analysis/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  Stats Analysis
+//
+//  Created by User on 3/14/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+    
+    printf("Hello, World!\n");
+    return 0;
+}
+
